049aa79b433920ecac3eb04b9d111437403242d8
Diego <diegosayron@gmail.com>
2024-07-28 11:52:08 -0300

    ok

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+
+import express from 'express';
+
 function Home() {
     return "<h1>Teste</h1>";
 }
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+    res.send(Home());
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
 
 export default Home;
