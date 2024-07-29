e1fd8a0061603ae000f56de7643e9ae04fa5e129
Diego <diegosayron@gmail.com>
2024-07-29 01:00:50 -0300

    correçoes

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,6 @@
 
-// import express from 'express';
-
 function Home() {
     return <h1>Diego Sayron</h1>;
 }
 
-/* const app = express();
-const port = process.env.PORT || 3001;
-
-app.get('/', (req, res) => {
-    res.send(Home());
-});
-
-app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
-});
-*/
 export default Home;
