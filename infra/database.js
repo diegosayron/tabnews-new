7bf12506a0e80f3eed4b3fb39a2a654a37a6d7b5
Diego Sayron <diegosayron@gmail.com>
2024-10-31 20:54:27 -0300

    Added environment variables in database.js and compose.yaml

--- infra/database.js
+++ infra/database.js
@@ -2,11 +2,11 @@ import { Client } from 'pg';
 
 async function query(queryObject) {
     const client = new Client({
-        host: 'localhost',
-        port: 5433,
-        user: 'postgres',
-        password: '..Br11dg3..',
-        database: 'postgres'
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
     });
     await client.connect();
     const result = await client.query(queryObject)   //vamos passar um queryObject, que vem lá da assinatura do método  e vem pela função query.
@@ -14,7 +14,6 @@ async function query(queryObject) {
     await client.end(); //para finalizar a conexão          
     return result;
 }
-
 
 export default {
     query: query,
