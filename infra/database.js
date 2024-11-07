5e02fff9b35e7d3f241b37e703e83c0fbdffeecc
Diego Sayron <diegosayron@gmail.com>
2024-11-07 20:30:13 -0300

    Production database added

--- infra/database.js
+++ infra/database.js
@@ -7,19 +7,22 @@ async function query(queryObject) {
         user: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
         database: process.env.POSTGRES_DB,
+        connectionTimeoutMillis: 5000, // Timeout para a conexão
     });
     try {
         await client.connect();
+        console.log("Conectado ao banco de dados com sucesso!");
         const result = await client.query(queryObject);   //vamos passar um queryObject, que vem lá da assinatura do método  e vem pela função query.
                                                          //E guardaremos na variável result
         return result;
     } 
     catch(error) {
-        console.log(error);
+        console.error("Erro ao conectar ou executar a query:", error);
         throw error;
     }
     finally {
         await client.end(); //para finalizar a conexão          
+        console.log("Conexão encerrada.");
     }
 }
 
@@ -27,4 +30,3 @@ async function query(queryObject) {
 export default {
     query: query,
 };
-
