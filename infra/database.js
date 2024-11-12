9da679a8dcfe0376789be1f6e8c152b4c6f7d7b9
Diego <diegosayron@gmail.com>
2024-11-12 01:45:36 -0300

    SSL OK PARA DEVELOPMENT E PRODUCTION

--- infra/database.js
+++ infra/database.js
@@ -7,9 +7,7 @@ async function query(queryObject) {
         user: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
         database: process.env.POSTGRES_DB,
-        ssl:{
-            rejectUnauthorized: false // Para evitar problemas com certificados autoassinados
-        },
+        ssl: process.env.NODE_ENV === 'development' ? false: true,
     });
     
     /*console.log(process.env.POSTGRES_HOST,
