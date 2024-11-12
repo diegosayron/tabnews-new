31a192b65a7ded970648a08e281b6b816f353c33
Diego <diegosayron@gmail.com>
2024-11-12 01:09:54 -0300

    logging 1

--- infra/database.js
+++ infra/database.js
@@ -8,23 +8,29 @@ async function query(queryObject) {
         password: process.env.POSTGRES_PASSWORD,
         database: process.env.POSTGRES_DB,
     });
-    console.log(process.env.POSTGRES_HOST,
+    
+    /*console.log(process.env.POSTGRES_HOST,
         process.env.POSTGRES_PORT,
         process.env.POSTGRES_USER,
         process.env.POSTGRES_PASSWORD,
         process.env.POSTGRES_DB);
+        */
+
     try {
         await client.connect();
+        console.log("Conectou ao banco");
         const result = await client.query(queryObject);   //vamos passar um queryObject, que vem lá da assinatura do método  e vem pela função query.
                                                          //E guardaremos na variável result
+        console.log("Query Ok");
         return result;
     } 
     catch(error) {
-        console.log(error);
+        console.log("Ocorreu um erro: " + error);
         throw error;
     }
     finally {
-        await client.end(); //para finalizar a conexão          
+        await client.end(); //para finalizar a conexão     
+        console.log("Conexão fechada com sucesso!");     
     }
 }
 
