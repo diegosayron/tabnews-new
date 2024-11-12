9e3e62cb146bbb6ecfa9a13c90ebe8a50d896878
Diego <diegosayron@gmail.com>
2024-11-12 00:01:05 -0300

    ssl ok

--- infra/database.js
+++ infra/database.js
@@ -7,25 +7,20 @@ async function query(queryObject) {
         user: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
         database: process.env.POSTGRES_DB,
-        connectionTimeoutMillis: 10000, // Timeout para a conexão
-        /*ssl: {
-            rejectUnauthorized: false // Para evitar problemas com certificados autoassinados
-        }*/
+    ssl: true,
     });
     try {
         await client.connect();
-        console.log("Conectado ao banco de dados com sucesso!");
         const result = await client.query(queryObject);   //vamos passar um queryObject, que vem lá da assinatura do método  e vem pela função query.
                                                          //E guardaremos na variável result
         return result;
     } 
     catch(error) {
-        console.error("Erro ao conectar ou executar a query:", error);
+        console.log(error);
         throw error;
     }
     finally {
         await client.end(); //para finalizar a conexão          
-        console.log("Conexão encerrada.");
     }
 }
 
@@ -33,3 +28,4 @@ async function query(queryObject) {
 export default {
     query: query,
 };
+
