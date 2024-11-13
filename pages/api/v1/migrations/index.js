55f9f62ca17ccf90a9a2820a2e165756cde4cedf
Diego <diegosayron@gmail.com>
2024-11-13 15:45:19 -0300

    Building migrations endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,5 +1,18 @@
-import database from 'infra/database.js';
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
 
 export default async function migrations(request, response) {
-    response.status(200).json({});
+    //DATA E HORA, se precisar
+    //const updatedAt = new Date().toISOString();
+
+    const migrations = await migrationRunner({
+        databaseUrl: process.env.DATABASE_URL,
+        dryRun: true,
+        dir: join("infra", "migrations"),
+        direction: "up",
+        verbose: true,
+        migrationsTable: "pgmigrations",
+    });
+
+    response.status(200).json(migrations);
 }
--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -2,4 +2,8 @@ test("Get to api/v1/migrations must return 200", async () => {
     const response = await fetch("http://localhost:3000/api/v1/migrations");
 
     expect(response.status).toBe(200);
+
+    const responseBody = await response.json();
+    //espero receber uma lista de migrations que estão pendentes. Sendo uma lista, é um array:
+    expect(Array.isArray(responseBody)).toBe(true);
 });
