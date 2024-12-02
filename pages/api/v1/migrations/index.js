9505ca077a36f2dc4e5a58e498a83f4f65dc8249
Diego <diegosayron@gmail.com>
2024-12-01 22:13:18 -0300

    Arquivo post.teste.js do endpoint migrations

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,17 +2,34 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
+    //console.log("LOG:" + request.method);
+    
     //DATA E HORA, se precisar
     //const updatedAt = new Date().toISOString();
 
-    const migrations = await migrationRunner({
-        databaseUrl: process.env.DATABASE_URL,
-        dryRun: true,
-        dir: join("infra", "migrations"),
-        direction: "up",
-        verbose: true,
-        migrationsTable: "pgmigrations",
-    });
+    if (request.method === "GET") {
+        const migrations = await migrationRunner({
+            databaseUrl: process.env.DATABASE_URL,
+            dryRun: true,
+            dir: join("infra", "migrations"),
+            direction: "up",
+            verbose: true,
+            migrationsTable: "pgmigrations",
+        });
+        return response.status(200).json(migrations);
+    } 
 
-    response.status(200).json(migrations);
+    if (request.method === "POST") {
+        const migrations = await migrationRunner({
+            databaseUrl: process.env.DATABASE_URL,
+            dryRun: true,
+            dir: join("infra", "migrations"),
+            direction: "up",
+            verbose: true,
+            migrationsTable: "pgmigrations",
+        });
+        return response.status(200).json(migrations);
+    }
+
+    return response.status(405).end();
 }
---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,11 @@
+test("POST to api/v1/migrations must return 200", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: 'POST'
+    });
+
+    expect(response.status).toBe(200);
+
+    const responseBody = await response.json();
+    //espero receber uma lista de migrations que estão pendentes. Sendo uma lista, é um array:
+    expect(Array.isArray(responseBody)).toBe(true);
+});
